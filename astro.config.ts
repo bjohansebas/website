b647de9b50af8dc4a94548ac3d831baad662beda
Sebastian Beltran <bjohansebas@gmail.com>
2024-06-01 20:08:15 -0500

    setup sitemap and improve build process

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,36 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en-US",
+          es: "es-ES",
+        },
+      },
+    }),
+  ],
+  build: {
+    inlineStylesheets: "always",
+  },
+  prefetch: true,
   output: "hybrid",
   adapter: vercel({
-    webAnalytics: { enabled: true },
+    webAnalytics: {
+      enabled: true,
+    },
   }),
+  site: "https://bjohansebas.vercel.app",
+  vite: {
+    build: {
+      cssMinify: "lightningcss",
+    },
+  },
 });
