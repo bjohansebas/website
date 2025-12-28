c7b137a82abe6de32e4f76c4a4740c81fb241c7a
Sebastian Beltran <bjohansebas@gmail.com>
2025-12-28 16:33:43 -0500

    feat: add og image generator

---
+++ src/pages/og/[...id].ts
@@ -0,0 +1,116 @@
+import fs from "fs/promises";
+import satori from "satori";
+import sharp from "sharp";
+
+export async function GET({
+  params,
+}: {
+  params: Record<string, string | string[]>;
+}) {
+  const rawId = params?.id;
+  const text = rawId ?? "";
+
+  const imgBuf = await fs.readFile(
+    new URL("../../../public/photo-460x460.png", import.meta.url),
+  );
+
+  const imgBase64 = imgBuf.toString("base64");
+
+  const GeinsS = await fs.readFile(
+    new URL(
+      "../../../src/assets/fonts/geist-sans-latin-500-normal.woff",
+      import.meta.url,
+    ),
+  );
+
+  const element = {
+    type: "div",
+    props: {
+      style: {
+        display: "flex",
+        width: "100%",
+        height: "100%",
+        background: "#0a0a0a",
+        color: "#f4f2f1",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "32px",
+        fontFamiliy: "'Geist Sans', sans-serif",
+        fontWeight: 500,
+        fontSize: 64,
+      },
+      children: [
+        {
+          type: "div",
+          props: {
+            style: {
+              position: "absolute",
+              top: 0,
+              width: "100%",
+              height: "100%",
+              zIndex: -1,
+              backgroundImage:
+                "radial-gradient(ellipse 80% 80% at 50% -20%,rgba(249, 112, 21, 0.3),hsla(22.4, 33.3%, 48.8%, 0))",
+            },
+          },
+        },
+        {
+          type: "img",
+          props: {
+            src: `data:image/png;base64,${imgBase64}`,
+            width: 160,
+            height: 160,
+            style: {
+              borderRadius: "50%",
+              display: "block",
+            },
+          },
+        },
+        {
+          type: "div",
+          props: {
+            style: {
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              fontSize: "42px",
+              textAlign: "center",
+              fontWeight: 400,
+              width: "70%",
+            },
+            children: text.slice(0, 100) || "",
+          },
+        },
+      ],
+    },
+  } as any;
+
+  const svg = await satori(element, {
+    width: 1200,
+    height: 630,
+    fonts: [
+      {
+        data: GeinsS,
+        name: "Geist Sans",
+        weight: 400,
+        style: "normal",
+      },
+    ],
+  });
+
+  // Convert SVG to PNG using sharp
+  const pngBuffer = await sharp(Buffer.from(svg))
+    .resize({ width: 1200, height: 600 })
+    .png()
+    .toBuffer();
+
+  return new Response(pngBuffer as BodyInit, {
+    headers: {
+      "Content-Type": "image/png",
+      "Content-Length": pngBuffer.length.toString(),
+      // 30 days = 30 * 24 * 60 * 60 = 2592000 seconds
+      "Cache-Control": "public, max-age=2592000, immutable",
+    },
+  });
+}
